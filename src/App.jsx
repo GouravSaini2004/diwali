354901d7870d3e67459c8436e222c374678a3b7b
GouravSaini <113535932+GouravSaini2004@users.noreply.github.com>
2024-10-31 08:18:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,4 @@
-// import { useState } from "react"
 
-
-// function App() {
-
-//   const [color ,setColor]= useState("olive")
-  
-//   return (
-//     <div className="w-full h-screen duration-200" style={{backgroundColor: color}}>
-//       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-//         <div className="flex flex-wrap justify-center bg-white gap-3 shadow-lg px-3 py-2 rounded-2xl ">
-//           <button 
-//           onClick={() =>(setColor("red"))}
-//           className="outline-none px-4 py-1 rounded-full text-white shadow-sm" style={{backgroundColor: "red"}}>
-//             red
-//           </button>
-//           <button 
-//           onClick={() =>(setColor("green"))}
-//           className="outline-none px-4 py-1 rounded-full text-white shadow-sm" style={{backgroundColor: "green"}}>
-//             green
-//           </button>
-//           <button 
-//           onClick={() =>(setColor("blue"))}
-//           className="outline-none px-4 py-1 rounded-full text-white shadow-sm" style={{backgroundColor: "blue"}}>
-//             blue
-//           </button>
-//           <button 
-//           onClick={() =>(setColor("yellow"))}
-//           className="outline-none px-4 py-1 rounded-full text-white shadow-sm" style={{backgroundColor: "yellow"}}>
-//             yellow
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default App
-
-// App.js
-// App.js
 import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
@@ -47,19 +7,16 @@ import 'slick-carousel/slick/slick-theme.css';
 function App() {
   const wishes = [
     {
-      text: "Wishing you a Diwali full of joy and prosperity!",
+      text: "Haan byyy mittar!!",
       img: "https://i.pinimg.com/originals/a8/2b/66/a82b6647451a1119ce269e1b7beb586a.jpg", // Replace with your image URL
     },
+   
     {
-      text: "May this Diwali light up your life with happiness and success.",
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJEZ4rKjDVwnlUdmnYy0nguwYYrt26gfkBJg&s", // Replace with your image URL
-    },
-    {
-      text: "Happy Diwali! May your life be as colorful as the lights of Diwali.",
+      text: "kya hal han!",
       img: "https://t3.ftcdn.net/jpg/09/58/26/42/360_F_958264245_hxL0uU1KoTaefpOJ1L3c0HghmbOGeYpW.jpg", // Replace with your image URL
     },
     {
-      text: "Wishing you and your family a very happy and prosperous Diwali!",
+      text: "happy Diwali!",
       img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwG_k-5BF5JFMywnzQNonK1xbflofSw1uwMw&s", // Replace with your image URL
     },
     {
